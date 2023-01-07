8497825b0942a0d00a013c2bac968da31f861790
Vinicius Carvalho <tech@vinicarvalho.com>
2023-01-07 20:02:16 -0300

    Renamed files

--- src/main/java/com/vinicarvalho/erudio/Startup.java
+++ src/main/java/com/vinicarvalho/erudio/Startup.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErudioApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErudioApplication.class, args);
+
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
